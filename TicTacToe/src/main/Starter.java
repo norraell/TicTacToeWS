e17064cd8f63637f8456c42881eeface79621522
Jonathan <Jonathan@Hasenburg.de>
2014-03-13 19:22:41 +0100

    Start Methode hinzugefügt

---
+++ TicTacToe/src/main/Starter.java
@@ -0,0 +1,12 @@
+package main;
+
+import controll.Control;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		Control c = new Control();
+		System.out.println("Peter Pan");
+	}
+
+}
